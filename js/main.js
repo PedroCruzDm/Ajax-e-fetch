08e066cc610e1e7df208a391eff08481b1cbc899
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2024-12-07 12:32:00 -0300

    arrumando a estrutura

--- js/main.js
+++ js/main.js
@@ -1,33 +1,22 @@
+let prometa_completa = (response) => {
+    console.log(response.text().then((data) => {
+    document.getElementById('resultado').innerHTML = data;
+  
+    }))
+};
 
+let clickbtn = () => {
+    fetch('main.php').then(prometa_completa);
+}
 
-window.onload = () => { //executa o código após o carregamento da página
-    
-    let prometa_completa = (response) => {
-        console.log(response.text().then((data) => {
-            document.getElementById('conteudo').innerHTML = data;
-
-    }));
-
-
-    let btn_iniciar_func = document.addEventListener('DOMContentLoaded', (event) => {
-        console.log('DOM completamente carregado e analisado');
-
+let btn_iniciar= document.addEventListener('DOMContentLoaded', (event) => {
+    window.document.getElementById('btn-inicial').addEventListener('click', (event) => {
+        window.document.getElementById('btn-inicial').style.display = 'none';
+        window.document.body.style.backgroundImage.search('https://www.google.com/imgres?q=josh%20hutcherson%20meme&imgurl=https%3A%2F%2Fplatform.polygon.com%2Fwp-content%2Fuploads%2Fsites%2F2%2Fchorus%2Fuploads%2Fchorus_asset%2Ffile%2F25125289%2Fvlcsnap_2023_12_01_10h37m31s394.jpg%3Fquality%3D90%26strip%3Dall%26crop%3D9.1276041666667%2C1.5740740740741%2C81.171875%2C96.203703703704&imgrefurl=https%3A%2F%2Fwww.polygon.com%2F23984032%2Fjosh-hutcherson-whistle-edit-meme-trend-explained&docid=JPzaDEN4N5CLGM&tbnid=cmrHyJ1wQWZotM&vet=12ahUKEwj6n8TI-ZGKAxWBHrkGHaoBEHUQM3oECBIQAA..i&w=1555&h=1037&hcb=2&ved=2ahUKEwj6n8TI-ZGKAxWBHrkGHaoBEHUQM3oECBIQAA')
         fetch('main.php').then(prometa_completa);
-        window.document.getElementById('btn-inicial').addEventListener('click', (event) => {
 
-        //    window.document.getElementById('btn-inicial').style.display = 'none';
-            const overlay = document.getElementsByTagName('body'); //estilizando com css :D
-           
-            overlay.style.animation.playstate = 'running';
-            setTimeout(function() {
-               overlay.style.animation.playstate = 'stopped'; 
-              }, 2000);
-            overlay.style.opacity = 1;
-            
-            
-            
-            
-        });
     });
-}
-}
+});
+
+
+window.onload = btn_iniciar;
--- main.php
+++ main.php
@@ -1,17 +1,14 @@
+<?php sleep(1); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atividade</title>
     <link rel="stylesheet" href="./css/main.css">
-    <script src="./js/main.js"></script>
 </head>
-<body id="overlay">
-    <main>
-        <button id="btn-inicial">Iniciar</button>
-        <br>
-        <p id="resultado"></p>
-    </main>
+<body>
+    <?php sleep(2); ?>
+    <iframe src="https:/www.youtube.com/embed/BbeeuzU5Qc8?autoplay=1&enable_js=1" frameborder="0" id="V_Youtube" allow="autoplay"></iframe>
 </body>
 </html>
